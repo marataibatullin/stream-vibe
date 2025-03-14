02d816a8d7f601d792757796ea95248f587ce4e4
marataibatullin <marat.aibatullin@gmail.com>
2025-03-14 20:24:45 +0700

    fix(favicons): исправлена ошибка с фавиконками при деплое на ghpages

--- src/global.jsx
+++ src/global.jsx
@@ -2,6 +2,10 @@ import '@/styles'
 import Content from "@/layouts/Content"
 import Footer from "@/layouts/Footer"
 import Header from "@/layouts/Header"
+import appleTouchIcon from '@/assets/favicons/apple-touch-icon.png'
+import favicon32 from '@/assets/favicons/favicon-32x32.png'
+import favicon16 from '@/assets/favicons/favicon-16x16.png'
+import manifest from '@/assets/favicons/site.webmanifest'
 import { Head } from "minista"
 
 export default function (props) {
@@ -16,10 +20,10 @@ export default function (props) {
       <Head htmlAttributes={{ lang: 'en' }}>
         <title>Stream Vibe | {title}</title>
         <script src="/src/main.js" type='module'/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+        <link rel="manifest" href={manifest} />
       </Head>
       <Header url={url} />
       <Content>
